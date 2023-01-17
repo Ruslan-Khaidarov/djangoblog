dd7dce58f852e1ea6a438c8750379d0b9b026dc0
ruslnkhdrv <ruslnkhdrv@gmail.com>
2023-01-17 20:06:56 +0300

    create new files in models

--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+SECRET_KEY = 'django-insecure-mt+gx$hq8b^zbw1d8155%ze7-kh@*t3&o@cv+!#@fs&h*e+-!t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
