eaffcdc4ccf083bccd905e7d8f2a9e2ea25df0c8
ruslnkhdrv <ruslnkhdrv@gmail.com>
2023-01-25 08:20:37 +0300

    add css styles and images

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -7,6 +7,8 @@ class Post(models.Model):
     description = models.TextField('Текст записи')
     autor = models.CharField('Имя автора', max_length=100)
     date = models.DateField('Дата публикации')
+    img = models.ImageField(
+        ("Изображение"), upload_to='image/%Y')
 
     def __str__(self):
         return f'{self.title},{self.autor}'
